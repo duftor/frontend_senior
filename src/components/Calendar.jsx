9c458ca3e6aa645516016800aafdefa76ca4285c
Victor Dufayet <duftor@outlook.fr>
2023-10-13 21:21:26 +0200

    ✨ Implement resize event

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -2,9 +2,54 @@ import React from "react"
 import styled from "styled-components"
 import Typo from "./reusable-ui/Typo"
 import { decimalToTime, generateHours } from "../utils/time"
+import { useEffect } from "react"
+import { useState } from "react"
+import { useRef } from "react"
+import data from "../data/input.json"
+
+// function debounce(fn, ms) {
+// 	let timer
+// 	return (_) => {
+// 		clearTimeout(timer)
+// 		timer = setTimeout((_) => {
+// 			timer = null
+// 			fn.apply(this, arguments)
+// 		}, ms)
+// 	}
+// }
 
 export default function Calendar() {
+	// const [dimensions, setDimensions] = useState({
+	// 	height: window.innerHeight,
+	// 	width: window.innerWidth,
+	// })
+
 	const hours = generateHours(9, 21)
+
+	// useEffect(() => {
+	// 	const debouncedHandleResize = debounce(function handleResize() {
+	// 		setDimensions({
+	// 			height: window.innerHeight,
+	// 			width: window.innerWidth,
+	// 		})
+	// 	}, 1000)
+
+	// 	window.addEventListener("resize", debouncedHandleResize)
+
+	// 	return (_) => {
+	// 		window.removeEventListener("resize", debouncedHandleResize)
+	// 	}
+	// }, [])
+
+	const dayRef = useRef(null)
+	const [dimensions, setDimensions] = useState({ height: 0, width: 0 })
+
+	useEffect(() => {
+		setDimensions({
+			height: dayRef.current.offsetHeight,
+			width: dayRef.current.offsetWidth,
+		})
+	}, [])
 
 	return (
 		<CalendarStyled>
@@ -20,8 +65,16 @@ export default function Calendar() {
 					))}
 				</div>
 				<div className="calendar">
-					<div className="day">
-						<Typo variant="body1">Lundi</Typo>
+					<div className="day" ref={dayRef}>
+						<Typo variant="body1">
+							Lundi <br />{" "}
+							<div>
+								{dimensions.width} x {dimensions.height}
+							</div>
+							{/* <div>
+								Rendered at {dimensions.width} x {dimensions.height}
+							</div> */}
+						</Typo>
 					</div>
 					<div className="day">
 						<Typo variant="body1">Mardi</Typo>
@@ -98,7 +151,8 @@ const CalendarStyled = styled.div`
 		gap: 12px;
 
 		.day {
-			background: red;
+			/* background: red; */
+			border: 1px solid black;
 			flex: 1;
 			display: flex;
 			text-align: center;
