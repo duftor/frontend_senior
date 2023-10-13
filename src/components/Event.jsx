02a7afd82c4f7cb9a795d3ed45dd00ea64e44e11
Victor Dufayet <duftor@outlook.fr>
2023-10-13 22:35:51 +0200

    ✨ Place Event to the good position

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,21 +1,32 @@
 import React from "react"
 import styled from "styled-components"
 import Typo from "./reusable-ui/Typo"
+import { timeStringToDecimal } from "../utils/time"
 
 export default function Event({ id, start, duration }) {
-	const timeScale = 12
-	const percentPerHour = 100 / timeScale
+	const startTime = 9
+	const endTime = 21
+	const timeScale = endTime - startTime
 
+	const percentPerHour = 100 / timeScale
 	const percentageHeight = percentPerHour * (duration / 60)
-	console.log(percentageHeight)
+
+	const percentageTop =
+		((timeStringToDecimal(start) - startTime) / (endTime - startTime)) * 100
+
+	console.log(percentageTop)
+
 	return (
-		<EventStyled height={percentageHeight}>
+		<EventStyled height={percentageHeight} top={percentageTop}>
 			<Typo variant="body1">Id : {id}</Typo>
 		</EventStyled>
 	)
 }
 
 const EventStyled = styled.div`
+	box-sizing: border-box;
+	position: relative;
+	top: ${({ top }) => top + "%"};
 	background: #d5f8ef;
 	border: 1px solid #13b789;
 
