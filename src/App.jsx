c3dace3cbf96397962709c6a4e42ff788d101545
Victor Dufayet <duftor@outlook.fr>
2023-10-14 14:50:17 +0200

    ✨ App > check if two events overlaps

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,43 @@ import Calendar from "./components/Calendar"
 import Event from "./components/Event"
 import data from "./data/input.json"
 import { useState } from "react"
+import { timeStringToDecimal } from "./utils/time"
+
+const convertEventIntoInterval = (event) => {
+	const start = timeStringToDecimal(event.start)
+	return {
+		start,
+		end: start + event.duration / 60,
+	}
+}
+
+const isOverlapping = (interval1, interval2) => {
+	const start = Math.max(interval1.start, interval2.start)
+	const end = Math.min(interval1.end, interval2.end)
+	return start < end
+}
 
 function App() {
 	const [events, setEvents] = useState(data)
 
-	const { id, duration, start } = events[1]
+	const e1 = convertEventIntoInterval(events[15])
+	const e2 = convertEventIntoInterval(events[13])
+
+	console.log(isOverlapping(e1, e2))
+
 	return (
 		<AppStyled>
 			{/* <Calendar /> */}
-			<Event id={id} duration={duration} start={start} />
+			{events.map(({ id, duration, start }) => (
+				<Event
+					key={id}
+					id={id}
+					duration={duration}
+					start={start}
+					// width={eventWidths[id]}
+					// left={eventLeftValues[id]}
+				/>
+			))}
 		</AppStyled>
 	)
 }
@@ -19,11 +47,12 @@ function App() {
 export default App
 
 const AppStyled = styled.div`
+	position: relative;
 	/* background-color: #f7f7f7; */
 	background-color: #ffffff;
 	width: 100vw;
 	height: 100vh;
 
 	box-sizing: border-box;
-	padding: 40px;
+	/* padding: 40px; */
 `
