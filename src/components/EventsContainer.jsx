cd93f22282d6ba03f7e8352104b99b738ff97a0d
Victor Dufayet <duftor@outlook.fr>
2023-10-16 00:10:01 +0200

    ✨ Create theme for colors

--- src/components/EventsContainer.jsx
+++ src/components/EventsContainer.jsx
@@ -2,50 +2,7 @@ import React from "react"
 import EventsGroup from "./EventsGroup"
 import styled from "styled-components"
 import { convertEventIntoInterval, groupIntervals } from "../utils/intervals"
-
-// Colors for groups
-const colors = [
-	{
-		light: "#fbf8cc",
-		dark: "#b39617",
-	},
-	{
-		light: "#FDE4CF",
-		dark: "#BF6114",
-	},
-	{
-		light: "#FFCFD2",
-		dark: "#BA1F2A",
-	},
-	{
-		light: "#F1C0E8",
-		dark: "#BA1A9C",
-	},
-	{
-		light: "#CFBAF0",
-		dark: "#5C19C7",
-	},
-	{
-		light: "#A3C4F3",
-		dark: "#1C62C2",
-	},
-	{
-		light: "#90DBF4",
-		dark: "#1B9BC6",
-	},
-	{
-		light: "#8EECF5",
-		dark: "#16B1BF",
-	},
-	{
-		light: "#98F5E1",
-		dark: "#19C19C",
-	},
-	{
-		light: "#B9FBC0",
-		dark: "#19C22A",
-	},
-]
+import { colors } from "./theme"
 
 export default function EventsContainer({ events, containingInterval }) {
 	const intervals = []
